8ae67dc516ffdd1f7f1b11c17f2bb4da64499a10
Shashank Malik <shashankmalik2203@gmail.com>
2024-03-11 11:34:51 +0530

    minor change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-//HOMEPAGE GET CALL
+/*HOMEPAGE GET CALL*/
 app.get ("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "public", "homepage", "homepage.html"));
 });
